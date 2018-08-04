acac14f2cba4a5c923d0f8942a2944189da22140
pythonengineer <pythonengineer@tuta.io>
2018-08-04 18:46:17 -0400

    general: Add basic level loading/saving and pos/hpr/scale editing

--- leveleditor/LevelEditor.py
+++ leveleditor/LevelEditor.py
@@ -1,14 +1,20 @@
 from pandac.PandaModules import *
 
 from direct.showbase.DirectObject import DirectObject
+from direct.directtools.DirectGlobals import *
 from direct.directnotify.DirectNotifyGlobal import directNotify
 
-from tkFileDialog import askdirectory
+from tkFileDialog import askopenfilename, askdirectory
 from Tkinter import *
 from Pmw import MegaToplevel, MenuBar, ComboBox
 from Pmw import NoteBook
 
-import os
+from libpandaworld.WorldCreator import WorldCreator
+
+from getpass import getuser
+from json import dumps
+
+import time, os
 
 class LevelEditor(NodePath, DirectObject):
     notify = directNotify.newCategory('LevelEditor')
@@ -17,14 +23,205 @@ class LevelEditor(NodePath, DirectObject):
         NodePath.__init__(self, 'LevelEditor')
 
         self.modelPaths = set()
+        self.nodePaths = {}
 
         self.window = LevelEditorWindow(self)
         self.window['title'] = 'libpandaworld Level Editor'
 
+        self.hubParent = 'null'
+
+        self.worldCreator = WorldCreator(self)
+
+        self.actionEvents = [
+            ('DIRECT_selectedNodePath', self.selectedNodePathHook),
+            ('arrow_left', self.keyboardXformSelected, ['left', 'xlate']),
+            ('arrow_right', self.keyboardXformSelected, ['right', 'xlate']),
+            ('arrow_up', self.keyboardXformSelected, ['up','xlate']),
+            ('arrow_down', self.keyboardXformSelected, ['down','xlate']),
+            ('control-arrow_left', self.keyboardXformSelected, ['left', 'rotate']),
+            ('control-arrow_right', self.keyboardXformSelected, ['right', 'rotate']),
+            ('control-arrow_up', self.keyboardXformSelected, ['up', 'rotate']),
+            ('control-arrow_down', self.keyboardXformSelected, ['down', 'rotate'])]
+
+        self.selectedNode = None
+
+        self.lastAngle = 0.0
+
         base.direct.enable()
+
+        base.camLens.setNear(1.0)
+        base.camLens.setFar(3000)
+        base.direct.camera.setPos(0, -10, 10)
 
         self.reparentTo(base.direct.group)
         self.show()
+
+        for event in self.actionEvents:
+            if len(event) == 3:
+                self.accept(event[0], event[1], event[2])
+            else:
+                self.accept(event[0], event[1])
+
+    def createRegion(self, regionData):
+        pass
+
+    def createLocation(self, locData):
+        pass
+
+    def createObject(self, obj, objType, parent, parentUid, objKey, dynamic, actualParentObj, **kwargs):
+        uid = objKey if type(objKey) == str else ''
+        if not uid: return
+
+        visual = obj.get('Visual') or {}
+        model = visual.get('Model')
+        if not model: return
+
+        if not model[-4:] == '.bam' and not model[-4:] == '.egg':
+            model += '.bam'
+
+        pos = VBase3(*obj.get('Pos'))
+        hpr = Point3(*obj.get('Hpr'))
+        scale = VBase3(*obj.get('Scale'))
+
+        self.window.showResources([model])
+
+        node = loader.loadModel(model)
+        node.setPos(pos)
+        node.setHpr(hpr)
+        node.setScale(scale)
+        node.reparentTo(render)
+
+        self.nodePaths[node] = [uid, node.getPos(), node.getHpr(), node.getScale(), set()]
+
+        traverser = self.nodeTraverser(node)
+        for n in traverser:
+            self.nodePaths[node][4].add(n)
+
+    def nodeTraverser(self, node):
+        yield node
+
+        for child in node.getChildren():
+            for result in self.nodeTraverser(child):
+                yield result
+
+    def selectedNodePathHook(self, nodePath):
+        if nodePath == self.selectedNode:
+            return
+
+        base.direct.deselect(nodePath)
+
+        for node, dataList in self.nodePaths.iteritems():
+            nodeSet = dataList[4]
+
+            for childNode in nodeSet:
+                if nodePath.getName() == childNode.getName():
+                    self.selectedNode = node
+                    base.direct.select(node)
+
+    def getUid(self):
+        return str(time.time()) + getuser()
+
+    def setLastAngle(self, angle):
+        self.lastAngle = angle
+
+    def getLastAngle(self):
+        return self.lastAngle
+
+    def keyboardRotateSelected(self, arrowDirection):
+        # Get snap angle
+        if base.direct.fShift:
+            oldSnapAngle = base.direct.grid.snapAngle
+            base.direct.grid.setSnapAngle(1.0)
+
+        snapAngle = base.direct.grid.snapAngle
+        # Compute new angle
+        if arrowDirection == 'left' or arrowDirection == 'up':
+            self.setLastAngle(self.getLastAngle() + snapAngle)
+        else:
+            self.setLastAngle(self.getLastAngle() - snapAngle)
+
+        if self.getLastAngle() < -180.0:
+            self.setLastAngle(self.getLastAngle() + 360.0)
+        elif self.getLastAngle() > 180.0:
+            self.setLastAngle(self.getLastAngle() - 360.0)
+
+        # Move selected objects
+        for selectedNode in base.direct.selected:
+            selectedNode.setHpr(self.getLastAngle(), 0, 0)
+
+        # Snap objects to grid and update DNA if necessary
+        self.updateSelectedPose(base.direct.selected.getSelectedAsList())
+        if base.direct.fShift:
+            base.direct.grid.setSnapAngle(oldSnapAngle)
+
+    def keyboardTranslateSelected(self, arrowDirection):
+        gridToCamera = base.direct.grid.getMat(base.direct.camera)
+        camXAxis = gridToCamera.xformVec(X_AXIS)
+        xxDot = camXAxis.dot(X_AXIS)
+        xzDot = camXAxis.dot(Z_AXIS)
+
+        # What is the current grid spacing?
+        if base.direct.fShift:
+            # If shift, divide grid spacing by 10.0
+            oldGridSpacing = base.direct.grid.gridSpacing
+            # Use back door to set grid spacing to avoid grid update
+            base.direct.grid.gridSpacing = base.direct.grid.gridSpacing / 10.0
+        deltaMove = base.direct.grid.gridSpacing
+
+        # Compute the specified delta
+        deltaPos = Vec3(0)
+        if abs(xxDot) > abs(xzDot):
+            if xxDot < 0.0:
+                deltaMove = -deltaMove
+            # Compute delta
+            if arrowDirection == 'right':
+                deltaPos.setX(deltaPos[0] + deltaMove)
+            elif arrowDirection == 'left':
+                deltaPos.setX(deltaPos[0] - deltaMove)
+            elif arrowDirection == 'up':
+                deltaPos.setY(deltaPos[1] + deltaMove)
+            elif arrowDirection == 'down':
+                deltaPos.setY(deltaPos[1] - deltaMove)
+        else:
+            if xzDot < 0.0:
+                deltaMove = -deltaMove
+            # Compute delta
+            if arrowDirection == 'right':
+                deltaPos.setY(deltaPos[1] - deltaMove)
+            elif arrowDirection == 'left':
+                deltaPos.setY(deltaPos[1] + deltaMove)
+            elif arrowDirection == 'up':
+                deltaPos.setX(deltaPos[0] + deltaMove)
+            elif arrowDirection == 'down':
+                deltaPos.setX(deltaPos[0] - deltaMove)
+
+        # Move selected objects
+        for selectedNode in base.direct.selected:
+            selectedNode.setPos(base.direct.grid,
+                                selectedNode.getPos(base.direct.grid) + deltaPos)
+
+        # Snap objects to grid and update DNA if necessary
+        self.updateSelectedPose(base.direct.selected.getSelectedAsList())
+        # Restore grid spacing
+        if base.direct.fShift:
+            # Use back door to set grid spacing to avoid grid update
+            base.direct.grid.gridSpacing = oldGridSpacing
+
+    def updateSelectedPose(self, selected):
+        for node in selected:
+            self.nodePaths[node][1] = node.getPos()
+            self.nodePaths[node][2] = node.getHpr()
+            self.nodePaths[node][3] = node.getScale()
+
+            self.worldCreator.updateObject(self.nodePaths[node][0], 'Pos', tuple(node.getPos()))
+            self.worldCreator.updateObject(self.nodePaths[node][0], 'Hpr', tuple(node.getHpr()))
+            self.worldCreator.updateObject(self.nodePaths[node][0], 'Scale', tuple(node.getScale()))
+
+    def keyboardXformSelected(self, arrowDirection, mode):
+        if mode == 'rotate':
+            self.keyboardRotateSelected(arrowDirection)
+        else:
+            self.keyboardTranslateSelected(arrowDirection)
 
     def loadResources(self):
         resourceDirectory = askdirectory()
@@ -56,6 +253,35 @@ class LevelEditor(NodePath, DirectObject):
         node = loader.loadModel(model)
         node.reparentTo(render)
 
+        self.nodePaths[node] = [self.getUid(), node.getPos(), node.getHpr(), node.getScale(), set()]
+
+        traverser = self.nodeTraverser(node)
+        for n in traverser:
+            self.nodePaths[node][4].add(n)
+
+    def loadWorld(self):
+        worldFilename = askopenfilename(
+            defaultextension = '.world',
+            filetypes = (('World Files', '*.world'),),
+            initialdir = 'worldData',
+            title = 'Load World File',
+            parent = self.window.component('hull'))
+        if not worldFilename:
+            return
+
+        worldFilename = os.path.basename(os.path.normpath(worldFilename))
+
+        self.worldCreator.makeRegion(worldFilename)
+
+    def saveWorld(self):
+        for name in self.worldCreator.fileDicts:
+            fileData = self.worldCreator.fileDicts[name]
+
+            name = name[:-6] + '.json'
+
+            with open(name, 'w+') as f:
+                f.write(dumps(fileData, indent=3))
+
 class LevelEditorWindow(MegaToplevel):
     notify = directNotify.newCategory('LevelEditorWindow')
 
@@ -81,6 +307,16 @@ class LevelEditorWindow(MegaToplevel):
                                  'Load resources from directory',
                                  label='Load resources',
                                  command=self.editor.loadResources)
+        self.menuBar.addmenuitem('File',
+                                 'command',
+                                 'Open world file',
+                                 label='Open',
+                                 command=self.editor.loadWorld)
+        self.menuBar.addmenuitem('File',
+                                 'command',
+                                 'Save world file',
+                                 label='Save',
+                                 command=self.editor.saveWorld)
 
         self.notebook = NoteBook(self.hull)
         self.notebook.pack(fill=BOTH, expand=1)
@@ -124,7 +360,16 @@ class LevelEditorWindow(MegaToplevel):
 
         self.editor.loadModel(self.currentModel)
 
-    def showResources(self):
-        for resource in self.editor.modelPaths:
+    def showResources(self, paths=[]):
+        if not paths:
+            paths = self.editor.modelPaths
+        else:
+            for path in paths:
+                self.editor.modelPaths.add(path)
+
+        for resource in paths:
             oldItems = self.modelSelector.get()
+            if isinstance(oldItems, str):
+                oldItems = ()
+
             self.modelSelector.setlist(oldItems + (resource,))
